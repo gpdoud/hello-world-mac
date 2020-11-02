63ea110a0ec5208cf477820d46f9b249243e832b
Greg Doud <gpdoud@Gregs-MacBook-Pro.local>
2020-11-01 22:42:22 -0500

    Initial commit

---
+++ HelloWorldLibrary/Lib.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HelloWorldLibrary {
+    public class Lib {
+        public static string About() {
+            return "About Hello World Library";
+        }
+    }
+}
---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HelloWorldProject {
+    class Program {
+        static void Main(string[] args) {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine($"{HelloWorldLibrary.Lib.About()}");
+        }
+    }
+}
