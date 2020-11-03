39922356db956b8f8ce655c0d906eedfe8d02e60
gpdoud <gpdoud@gmail.com>
2020-11-02 22:38:05 -0500

    * HelloWorldLibrary.csproj: Project file
    * Lib.cs: Added Get method to connect to Azure Db
    
    * Program.cs: Connected to Azure database

--- HelloWorldLibrary/Lib.cs
+++ HelloWorldLibrary/Lib.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace HelloWorldLibrary {
     public class Lib {
         public static string About() {
             return "About Hello World Library";
+        }
+        public static void Get() {
+            var connStr = "Server=tcp:prsbc30db.database.windows.net,1433;" +
+                            "Initial Catalog=PrsBc30Server_db;" +
+                            "Persist Security Info=False;" +
+                            "User ID=prsbc30admin;Password=prsbc30Il0v3cnk;" +
+                            "MultipleActiveResultSets=False;" +
+                            "Encrypt=True;TrustServerCertificate=False;" +
+                            "Connection Timeout=30;";
+            var conn = new SqlConnection(connStr);
+            conn.Open();
+            if(conn.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Could not open connection");
+            }
+            Console.WriteLine("Success!");
+            conn.Close();
         }
     }
 }
--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -3,6 +3,7 @@
 namespace HelloWorldProject {
     class Program {
         static void Main(string[] args) {
+            HelloWorldLibrary.Lib.Get();
             //Console.WriteLine("Hello World!");
             Console.WriteLine($"{HelloWorldLibrary.Lib.About()}");
         }
